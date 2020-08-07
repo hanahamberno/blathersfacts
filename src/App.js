de70bb9ff78103f5b996e67554a6608540e496e1
Hanah Amber No <hanahamberno@gmail.com>
2020-08-06 23:25:11 -0400

    Add random generator button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
+const App = () => { 
+  const [species, setSpecies] = React.useState("");
+  const [fact, setFact] = React.useState(""); 
+
+  const acnhAPI = async () => {
+    let arrayOfFacts = []; 
+    try {
+      const data = await axios.get("https://acnhapi.com/v1a/fish/");
+      arrayOfFacts = data.data;
+      console.log(arrayOfFacts); 
+    } catch (error) {
+      console.log(error)
+    }
+
+    try {
+      var min = Math.ceil(0); 
+      var max = Math.floor(79);
+      var rand = Math.floor(Math.random() * (max - min)) + min; 
+      setSpecies(arrayOfFacts[rand]["file-name"]);
+      setFact(arrayOfFacts[rand]["museum-phrase"]); 
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    acnhAPI(); 
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {species}
+      {fact}
+      <button onClick={acnhAPI}>GIMME FACT BOI</button>
     </div>
   );
-}
+}; 
+
 
 export default App;
