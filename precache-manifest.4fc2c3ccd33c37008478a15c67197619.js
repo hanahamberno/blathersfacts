2fa45edd92145d8b5a8fe6702fc899ab92fa89d8
Hanah Amber No <hanahamberno@gmail.com>
2020-08-18 00:19:48 -0400

    Updates

---
+++ precache-manifest.4fc2c3ccd33c37008478a15c67197619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8685d998d496ca1e709dff5ae3f23190",
+    "url": "/blathersfacts/index.html"
+  },
+  {
+    "revision": "8bb3dc025c2a3c7071db",
+    "url": "/blathersfacts/static/css/main.7f82f512.chunk.css"
+  },
+  {
+    "revision": "1e50b4ade6d90c464372",
+    "url": "/blathersfacts/static/js/2.2032dec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blathersfacts/static/js/2.2032dec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb3dc025c2a3c7071db",
+    "url": "/blathersfacts/static/js/main.7d784479.chunk.js"
+  },
+  {
+    "revision": "2f8dad050b3bbed718df",
+    "url": "/blathersfacts/static/js/runtime-main.89a37b0c.js"
+  }
+]);
